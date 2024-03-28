2b3405f5a2e05d464fc8ff36fb68ffd756a441dd
hank liu <hankliucode@gmail.com>
2024-03-28 21:47:10 +0800

    feat: Implement modern symmetric cipher

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,15 +1,27 @@
+# frozen_string_literal: true
+
+require 'base64'
+require 'rbnacl'
+
+# module for ModernSymmetricCipher
 module ModernSymmetricCipher
   def self.generate_new_key
-    # TODO: Return a new key as a Base64 string
+    # Return a new key as a Base64 string
+    Base64.strict_encode64(RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes))
   end
 
   def self.encrypt(document, key)
-    # TODO: Return an encrypted string
-    #       Use base64 for ciphertext so that it is sendable as text
+    # Return an encrypted string
+    # Uses base64 for ciphertext so that it is sendable as text
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.strict_decode64(key))
+    Base64.strict_encode64(simple_box.encrypt(document))
   end
 
   def self.decrypt(encrypted_cc, key)
-    # TODO: Decrypt from encrypted message above
-    #       Expect Base64 encrypted message and Base64 key
+    # Decrypt from encrypted message above
+    # Expect Base64 encrypted message and Base64 key
+    key = Base64.strict_decode64(key)
+    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
+    simple_box.decrypt(Base64.strict_decode64(encrypted_cc))
   end
 end
