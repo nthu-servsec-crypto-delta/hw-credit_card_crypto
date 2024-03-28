5b5b742385043a74e9a1d59fd56f3418a173eab5
hank liu <hankliucode@gmail.com>
2024-03-28 22:02:14 +0800

    test: metaprogramming

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -10,42 +10,28 @@ describe 'Test card info encryption' do
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
                          'Soumya Ray', 'Visa')
-    @key = 3
-    @sym_key = ModernSymmetricCipher.generate_new_key
   end
   @testcases = {
-    Caesar: SubstitutionCipher::Caesar,
-    Permutation: SubstitutionCipher::Permutation,
-    DoubleTransposition: DoubleTranspositionCipher
+    Caesar: [SubstitutionCipher::Caesar, 3],
+    Permutation: [SubstitutionCipher::Permutation, 3],
+    DoubleTransposition: [DoubleTranspositionCipher, 3],
+    ModernSymmetric: [ModernSymmetricCipher, ModernSymmetricCipher.generate_new_key]
   }
 
-  @testcases.each do |name, mod|
+  @testcases.each do |name, value|
+    mod, key = value
     describe "Using #{name} cipher" do
       it 'should encrypt card information' do
-        enc = mod.encrypt(@cc.to_s, @key)
+        enc = mod.encrypt(@cc.to_s, key)
         _(enc).wont_equal @cc.to_s
         _(enc).wont_be_nil
       end
 
       it 'should decrypt text' do
-        enc = mod.encrypt(@cc.to_s, @key)
-        dec = mod.decrypt(enc, @key)
+        enc = mod.encrypt(@cc.to_s, key)
+        dec = mod.decrypt(enc, key)
         _(dec).must_equal @cc.to_s
       end
-    end
-  end
-
-  describe 'Using Modern Symmetric cipher' do
-    it 'should encrypt card information' do
-      enc = ModernSymmetricCipher.encrypt(@cc.to_s, @sym_key)
-      _(enc).wont_equal @cc.to_s
-      _(enc).wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = ModernSymmetricCipher.encrypt(@cc.to_s, @sym_key)
-      dec = ModernSymmetricCipher.decrypt(enc, @sym_key)
-      _(dec).must_equal @cc.to_s
     end
   end
 end
